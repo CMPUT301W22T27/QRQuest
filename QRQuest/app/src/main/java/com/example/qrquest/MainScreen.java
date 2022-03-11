4f2c67ae1c7348575dd4c1ad220255e36319b7ee
Dave Goel <davegoel29@gmail.com>
2022-03-10 17:46:29 -0700

    merge intro and scanner

--- QRQuest/app/src/main/java/com/example/qrquest/MainScreen.java
+++ QRQuest/app/src/main/java/com/example/qrquest/MainScreen.java
@@ -1,7 +1,10 @@
 package com.example.qrquest;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +12,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -30,5 +37,48 @@ public class MainScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+
+    public void scanner(View view){
+        IntentIntegrator intentIntegrator = new IntentIntegrator(
+                MainScreen.this
+        );
+        intentIntegrator.setPrompt("For flash use volume up key");
+        intentIntegrator.setBeepEnabled(false);
+        intentIntegrator.setOrientationLocked(true);
+        intentIntegrator.setCaptureActivity(Capture.class);
+        intentIntegrator.initiateScan();
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (intentResult.getContents() != null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainScreen.this);
+            builder.setTitle("Result");
+
+            QRCode qrCode = new QRCode(intentResult.getContents());
+            String score = Integer.toString(qrCode.getScore());
+            builder.setMessage(score);
+
+
+//            builder.setMessage(intentResult.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+
+                }
+            });
+            builder.show();
+
+        }else {
+            Toast.makeText(getApplicationContext(), "OOPS... You did not scan anything", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }
