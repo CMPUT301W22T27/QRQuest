d3ab1fda2e4ba5e0d570a851521882a12bb71aa8
Mikal Kotadia <mikal@ualberta.ca>
2022-03-10 18:35:29 -0700

    cleaned up some comments

--- QRQuest/app/src/main/java/com/example/qrquest/QRCode.java
+++ QRQuest/app/src/main/java/com/example/qrquest/QRCode.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class QRCode {
-    //    private  String value;
     private String hash;
     private Integer score;
     // we should include the location here
