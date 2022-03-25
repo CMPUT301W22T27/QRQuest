89be40e00375629499db1763b0fdfa92d5c8e772
Mikal Kotadia <mikal@ualberta.ca>
2022-03-25 17:16:09 -0600

    added ability to take photo and display in the ScanSucess view

--- QRQuest/app/src/main/java/com/example/qrquest/ScanSuccess.java
+++ QRQuest/app/src/main/java/com/example/qrquest/ScanSuccess.java
@@ -22,9 +22,39 @@ public class ScanSuccess extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_success);
+        imageView = findViewById(R.id.pictureView);
+        takePictureButton = findViewById(R.id.photoButton);
+
         Bundle extras = getIntent().getExtras();
         String score = extras.getString("score");
         scoreDisplay = findViewById(R.id.scoreView);
         scoreDisplay.setText("Score: " + score);
+
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
     }
 }
