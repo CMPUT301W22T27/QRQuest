02804ef2b5d67e60f84a2506c87e37ec4ed3d463
Mikal Kotadia <mikal@ualberta.ca>
2022-03-31 22:56:09 -0600

    added commented out code trying to get the name instead of the hash. struggling with this.

--- QRQuest/app/src/main/java/com/example/qrquest/OtherPlayerQRCodeCount.java
+++ QRQuest/app/src/main/java/com/example/qrquest/OtherPlayerQRCodeCount.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Debug;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,14 +15,18 @@ import android.widget.TextView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
 
 public class OtherPlayerQRCodeCount extends AppCompatActivity {
     String username;
+    String currentQRrCodeName;
     FirebaseFirestore db;
     public ListView qrCodeList;
     public ArrayList<String> qrDataList = new ArrayList<String>();
@@ -51,10 +56,14 @@ public class OtherPlayerQRCodeCount extends AppCompatActivity {
 
                     otherPlayerUsername.setText(username+"'s QRCodes");
                     for (int i = 0; i < string.length; i++) {
+//                        getQRCodeName(string[i]);
+//                        qrNameList.add(currentQRrCodeName);
                         qrDataList.add(string[i]);
+
                     }
                     qrCodeList = findViewById(R.id.OtherPlayQRCodeList);
                     qrCodeAdapter = new ArrayAdapter<String>(OtherPlayerQRCodeCount.this,android.R.layout.simple_list_item_1, qrDataList);
+//                    qrCodeAdapter = new ArrayAdapter<String>(OtherPlayerQRCodeCount.this,android.R.layout.simple_list_item_1, qrNameList);
                     qrCodeList.setAdapter(qrCodeAdapter);
                     qrCodeList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                         @Override
@@ -69,4 +78,18 @@ public class OtherPlayerQRCodeCount extends AppCompatActivity {
             }
         });
     }
+
+//    public void getQRCodeName(String hashValue) {
+//        CollectionReference qrCodeCollection = db.collection("QRCodes");
+//        qrCodeCollection.document(hashValue).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                currentQRrCodeName = (String) task.getResult().get("name");
+//            }
+//        });
+//        return;
+////        qrCodeDocument.get().getResult();
+////        String testing = qrCodeDocument.get().getResult().getString("name");
+////        return qrCodeDocument.get().getResult().getString("name");
+//    }
 }
